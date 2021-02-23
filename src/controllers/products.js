aa3c3ba191920e0274cf13338ec1f468b778607a
Thiago Medeiros <thiagocontaparatestes@gmail.com>
2021-02-23 14:55:22 +0000

    adicionando metodo get

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,11 +1,58 @@
 const ProductsModel = require('../models/products')
 
 async function get(req, res) {
-  const products = await ProductsModel.find()
+  const { id } = req.params
+
+  const obj = id ? { _id: id } : null
+
+  const products = await ProductsModel.find(obj)
 
   res.send(products)
 }
 
+async function post(req, res) {
+  const {
+    name,
+    brand,
+    price,
+  } = req.body
+
+  const product = new ProductsModel({
+    name,
+    brand,
+    price,
+  })
+
+  product.save()
+
+  res.send({
+    message: 'success'
+  })
+}
+
+async function put(req, res) {
+  const { id } = req.params
+
+  const {
+    name,
+    brand,
+    price,
+  } = req.body
+
+  const product = await ProductsModel.findOneAndUpdate(id, {
+    name,
+    brand,
+    price,
+  }, { new: true, upsert: true })
+
+  res.send({
+    message: 'success'
+  })
+}
+
+
 module.exports = {
   get,
+  post,
+  put,
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 
 const ProductController = require('../controllers/products')
 
-router.get('/products', ProductController.get)
-// router.post('/products', ProductController.post)
-// router.put('/products/:id', ProductController.put)
+router.get('/products/:id?', ProductController.get)
+router.post('/products', ProductController.post)
+router.put('/products/:id', ProductController.put)
 // router.delete('/products/:id', ProductController.delete)
 
 module.exports = router
